d95ea5352f5d82af95b3589f9958d92465843aa1
nberk <nick.r.berk@gmail.com>
2016-02-20 13:58:25 -0800

    Adding list parameter to name lookup function

---
+++ LunchApp/test/test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+describe ('StringGeneration', function() {
+	var users;
+	beforeEach(function() {
+		users = [
+			{ name: 'Nick',phone: '+16026164854'},
+			{ name: 'Mandeep',phone: '+17174601902'},
+			{ name: 'Anurag', phone: '+14802367962'},
+			{ name: 'Ryan', phone: '+19723658656'}
+			];
+	}
+
+	describe ('#lookUpName(phoneNumber)', function () {
+		it ('should return the the name of the person given a phone number')
+	}
+}

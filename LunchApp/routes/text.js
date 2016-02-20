4961a8549c90404347421a5c227adae366686b9d
Ryan Phillips <RyanPPhillips10@gmail.com>
2016-02-20 01:46:58 -0800

    move sendText to bottom

--- LunchApp/routes/text.js
+++ LunchApp/routes/text.js
@@ -36,30 +36,7 @@ function sendPromptText(users,promptMessage)
    }
 }
 
-function sendText(phoneNumber, message){
-    client.sendMessage( {
 
-        to: phoneNumber, // Any number Twilio can deliver to
-        from: TwilioNumber, 
-        body: message // body of the SMS message
-
-    }, function(err, responseData) { //this function is executed when a response is received from Twilio
-
-        if (!err) { // "err" is an error received during the request, if any
-
-            // "responseData" is a JavaScript object containing data received from Twilio.
-            // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-            // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-
-            console.log(responseData.from); // outputs "+14506667788"
-            console.log(responseData.body); // outputs "word to your mother."
-
-        }
-        else {
-            console.log(err);
-        }
-    });
-}
 
 
 // ------------------------- Receiving Texts -----------------------------
@@ -96,5 +73,30 @@ router.post('/', function(req, res) {
 
     
 });
+
+function sendText(phoneNumber, message){
+    client.sendMessage( {
+
+        to: phoneNumber, // Any number Twilio can deliver to
+        from: TwilioNumber, 
+        body: message // body of the SMS message
+
+    }, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+        if (!err) { // "err" is an error received during the request, if any
+
+            // "responseData" is a JavaScript object containing data received from Twilio.
+            // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+            // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+            console.log(responseData.from); // outputs "+14506667788"
+            console.log(responseData.body); // outputs "word to your mother."
+
+        }
+        else {
+            console.log(err);
+        }
+    });
+}
 
 module.exports = router;

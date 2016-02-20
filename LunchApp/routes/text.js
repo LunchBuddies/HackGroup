f0cab2a6f18495232b47749d7dd1bd53ccd2b9d5
nberk <nick.r.berk@gmail.com>
2016-02-20 15:06:09 -0800

    Updating Time

--- LunchApp/routes/text.js
+++ LunchApp/routes/text.js
@@ -24,8 +24,8 @@ var confirmedAttendees = [];
     {phone: '+19723658656'}
 ];*/
 
-var promptTime = '00 05 15 * * 0-6';
-var confirmationTime = '00 07 15 * * 0-6'
+var promptTime = '00 10 15 * * 0-6';
+var confirmationTime = '00 12 15 * * 0-6'
 
 var promptMessage = 'Are you in for lunch at noon? Yes or No';
 var confirmationMessage = 'Confirmed, see you at noon!';

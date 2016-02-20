e55103630d1d1789aec06c0b06cb4f4cb9de218c
Ryan Phillips <RyanPPhillips10@gmail.com>
2016-02-20 13:43:07 -0800

    incomplete

--- LunchApp/routes/text.js
+++ LunchApp/routes/text.js
@@ -6,6 +6,18 @@ var client = require('twilio')(keys.TWILIO_ACCOUNT_SID, keys.TWILIO_AUTH_KEY);
 var router = express.Router();
 var CronJob = require('cron').CronJob;
 
+// for Mongo
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+//connect to db running on local box
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
 
 var TwilioNumber = '+14693400518'; // A number you bought from Twilio and can use for outbound communication
 

70ae3de9b07941f0e33bac9e54c53635057b5e94
Ryan Phillips <RyanPPhillips10@gmail.com>
2016-02-21 14:34:17 -0800

    comment out logging

--- LunchApp/routes/text.js
+++ LunchApp/routes/text.js
@@ -96,7 +96,7 @@ function logHistoryEvent (_eventType, _params) {
     historyEventToSend.save(function(err, thor) {
         if (err) 
         {
-            logHistoryEvent ('Error', err);
+            // logHistoryEvent ('Error', err);
             return console.error(err);
         }
         console.dir("----- logged 1 historical event");
@@ -119,7 +119,7 @@ function promptCronLogic ()  {
         }
         else
         {
-            logHistoryEvent ('Error', err);
+            // logHistoryEvent ('Error', err);
         }
     });
     
@@ -136,7 +136,7 @@ function promptCronLogic ()  {
         }  
         else
         {
-            logHistoryEvent ('Error', err);
+            // logHistoryEvent ('Error', err);
         }  
             
       // numAffected is the number of updated documents
@@ -158,7 +158,7 @@ function confirmCronLogic () {
             }
             else
             {
-                logHistoryEvent ('Error', err);
+                // logHistoryEvent ('Error', err);
             }
         }
     );
@@ -304,7 +304,6 @@ router.post('/', function(req, res) {
             console.log ('keyword' + keyword);
             if(keyword == "JOIN")
             {   
-
                 var insertUser = new user ({name:user, phone:req.body.From,group:'OENGPM', isGoing: false, isConfirmed:true });
                 insertUser.save (function (err, result) 
                 {
@@ -365,12 +364,12 @@ function sendText(phoneNumber, message, retry){
             // console.log(responseData.from + ' ' + responseData.body); // outputs "+14506667788"
             // console.log(responseData.body); // outputs "word to your mother."
             console.log('----- Sent text to ' + responseData.to + ': done')
-            logHistoryEvent ('SendText', {phoneNumber: responseData.to, message: message});
+            // logHistoryEvent ('SendText', {phoneNumber: responseData.to, message: message});
         }
         else {
             console.log(err);
             // If it was the first time failed, try again
-            logHistoryEvent ('Error', err);
+            // logHistoryEvent ('Error', err);
             if (retry)
             {
                 sendText (phoneNumber, message, False);

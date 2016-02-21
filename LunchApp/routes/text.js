67311623de9e0163f79cdd1e40eb38a74100c734
Anurag Gupta <agup006@gmail.com>
2016-02-21 07:38:04 +0000

    Adding for debuggin

--- LunchApp/routes/text.js
+++ LunchApp/routes/text.js
@@ -168,7 +168,7 @@ router.post('/', function(req, res) {
           || (new RegExp("YEA")).test(req.body.Body.toUpperCase())
           || (new RegExp("YA")).test(req.body.Body.toUpperCase()))
         {
-           
+           console.log('RAW REQUEST' + req);
             // PUT SAVE FUNCTION HERE
             var testConfirmationObj = new confirmation ({
                 phone: req.body.From,

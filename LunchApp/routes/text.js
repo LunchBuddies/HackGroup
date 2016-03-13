c0e9ba1d615f300595ef0361f91dc00119d13fe8
Ryan Phillips <RyanPPhillips10@gmail.com>
2016-03-13 13:50:05 -0700

    comment lines for testing 3

--- LunchApp/routes/text.js
+++ LunchApp/routes/text.js
@@ -445,7 +445,7 @@ router.post('/', function(req, res) {
                 // Update status of user to 
                 var conditionsForUpdateDB = { phone: req.body.From }
                   , updateForUpdateDB = { isGoing: true };
-                updateUserObject(conditionsForUpdateDB, updateForUpdateDB, strings.immediateYesResponsesMessages);
+                updateUserObject(conditionsForUpdateDB, updateForUpdateDB, {}, strings.immediateYesResponsesMessages);
             // }
             // else
             // {
